044ef44d1b6ea412d53396220db41f05e5f1730a
debadatta10pati <46395564+debadatta10pati@users.noreply.github.com>
2022-02-12 23:18:29 -0500

    Update KogitoApplication.java

--- src/main/java/com/training/KogitoApplication.java
+++ src/main/java/com/training/KogitoApplication.java
@@ -2,12 +2,20 @@ package com.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = { "com.example.**", "com.training.**", "org.kie.kogito.**", "http**" })
 public class KogitoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KogitoApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
 }
